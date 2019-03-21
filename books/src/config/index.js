8f780a2576401d2548f3cb89b5171d4702936e9c
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2019-03-21 16:54:03 +0300

    Update index.js

--- books/src/config/index.js
+++ books/src/config/index.js
@@ -1,8 +1,4 @@
-let DB_URI = "mongodb://localhost:27017/microservices";
-
-if (process.env.MONGO_DB_URI) {
-  DB_URI = process.env.MONGO_DB_URI;
-}
+let DB_URI = process.env.MONGO_DB_URI || "mongodb://localhost:27017/microservices";
 
 module.exports = {
   DB_URI
